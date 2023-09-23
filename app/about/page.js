b89f703c835b779fd34e75e5342b2387dd0263c4
Aditya paluskar <adityapaluskar@gmail.com>
2023-09-23 13:49:37 +0530

    More errors fixed

--- app/about/page.js
+++ app/about/page.js
@@ -3,36 +3,7 @@ import React from 'react'
 export default function page() {
   return (
     <div className='flex-col flex justify-center items-center p-6'>
-      <h1 className='text-4xl font-bold'>Exploring the Intersection of Tech and Creativity: Aditya Paluskar's Tech Blog</h1>
-      <p className='text-sm py-10'>Welcome to my Tech Blog, where I delve into the exciting world of technology and creativity. I'm Aditya Paluskar, a tech enthusiast, developer, and a firm believer in the power of innovation. In this blog, I'll be sharing insights, tips, and tutorials on various tech topics. Feel free to connect with me on Instagram, GitHub, and LinkedIn for more tech updates and discussions.</p>
-      <h1 className='pt-10'>Perks of exploring this blog :</h1>
-      <div className='py-5'>
-        <h3 className='text-sm font-bold'>The Art of Coding and Creativity</h3>
-        <p className='text-sm'>In today's tech-driven world, coding is not just about functionality but also about creativity. I'll discuss how coding can be an art form and how developers can infuse creativity into their projects.</p>
-      </div>
-      <div className='py-5'>
-        <h3 className='text-sm font-bold'>A Deep Dive into AI and Machine Learning</h3>
-        <p className='text-sm'>Explore the fascinating world of AI and machine learning. Ill share practical insights, tutorials, and the latest trends in artificial intelligence and how its shaping our future.</p>
-      </div>
-      <div className='py-5'>
-        <h3 className='text-sm font-bold'>Open Source: Collaborate and Contribute</h3>
-        <p className='text-sm'>GitHub is not just a code repository; its a platform for collaboration and contribution. Ill guide you through the world of open source and how you can be a part of it.</p>
-      </div>
-      <div className='py-5'>
-        <h3 className='text-sm font-bold'>The Future of Tech: Blockchain and Cryptocurrencies</h3>
-        <p className='text-sm'>Blockchain technology and cryptocurrencies are disrupting industries. Ill provide an overview, explore real-world applications, and discuss the potential impact on our daily lives.</p>
-      </div>
-      <div className='py-5'>
-        <h3 className='text-sm font-bold'> Building Your Tech Career</h3>
-        <p className='text-sm'>Whether youre a beginner or an experienced tech professional, advancing your career is crucial. Ill offer career advice, interview tips, and strategies to excel in the tech industry.</p>
-      </div>
-      <div className='flex flex-col justify-center items-center'>
-        <h1 className='pt-10 font-bold text-sm'>Connect with me</h1>
-        <a href='https://instagram.com/giggasad' target='_blank' className='text-sm'>Instagram</a>
-        <a href='https://www.linkedin.com/in/aditya-paluskar-285698265/' target='_blank' className='text-sm'>Linkedin</a>
-        <a href='https://github.com/PaluskarAditya' target='_blank' className='text-sm'>GitHub</a>
-      </div>
-      <p className='text-sm py-10'>Thank you for visiting my Tech Blog. Stay connected with me on Instagram, GitHub, and LinkedIn to join the conversation and explore the limitless possibilities of technology and creativity. Together, well navigate the ever-evolving tech landscape.</p>
+      <h1>about page</h1>
     </div>
   )
 }
