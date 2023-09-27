633e7ad283e8e3e8bf82e4dab82917a3df819a3f
aditya paluskar <adityapaluskar@gmail.com>
2023-09-27 19:07:07 +0530

    Link errors fixed

--- app/components/Blog.jsx
+++ app/components/Blog.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Blog({ img, text, url }) {
   return (
-    <Link href={url} className='flex flex-col w-full rounded-md h-96 border border-gray-600 cursor-pointer'>
+    <Link href={url} as={url} className='flex flex-col w-full rounded-md h-96 border border-gray-600 cursor-pointer'>
       <img src={img} className='rounded-t-md object-center object-cover flex-1 h-3/4' loading='lazy'/>
       <div className='p-3 flex justify-center items-center flex-1'>
         <p className='text-sm'>{text.slice(0, 300)}...</p>
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,15 +6,15 @@ export default function Navbar() {
     <nav data-aos="fade-down" className='flex justify-between items-center fixed top-0 left-0 right-0 text-white p-3 px-6'>
       <ul>
         <li>
-          <Link href={'/'} className='text-2xl font-bold'>Tech Blog Daily</Link>
+          <Link href={'/'} as={'/'} className='text-2xl font-bold'>Tech Blog Daily</Link>
         </li>
       </ul>
       <ul className='flex justify-between items-center'>
         <li className='pl-4'>
-          <Link href={'/blogs'} className='text-sm'>blogs</Link>
+          <Link href={'/blogs'} as={'/blogs'} className='text-sm'>blogs</Link>
         </li>
         <li className='pl-4'>
-          <Link href={'/about'} className='text-sm'>about</Link>
+          <Link href={'/about'} as={'/about'} className='text-sm'>about</Link>
         </li>
       </ul>
     </nav>
